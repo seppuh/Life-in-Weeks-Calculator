ca42e3e3f00a56892fc9674cc3a1155d98cd0add
seppuh <150061021+seppuh@users.noreply.github.com>
2023-12-18 20:17:12 +0100

    WIP

---
+++ script.js
@@ -0,0 +1,12 @@
+function until90YearsOld(currentAge) {
+    //calculate age to days
+    let ageInDays = 32850-(currentAge*365);
+    //calculate age to weeks
+    let ageInWeeks = 4680-(currentAge*52);
+    //calculate age to months
+    let ageInMonths = 1080-(currentAge*12);
+    //output total in days, weeks, months
+    console.log("You have " + ageInDays + " days, " + ageInWeeks + " weeks and "+ ageInMonths + " months left")
+}
+
+until90YearsOld(89);
